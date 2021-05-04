1df1d514c1733a7fa49aa8ec5ec006a5e6a3fcf2
juulioclf <juliolima731@gmail.com>
2021-05-03 22:34:25 -0400

    Update

---
+++ Exercicios/exercicios24.js
@@ -0,0 +1,10 @@
+/* 24:  Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while
+*/
+
+var i = 1
+while(i < 11){
+    console.log(`Hello World`)
+    i++
+}
+console.log('\n')
+
---
+++ Exercicios/exercicios25.js
@@ -0,0 +1,9 @@
+/* 25:  Escrever um programa para exibir os números de 1 até 50 na tela
+*/
+
+var j = 1
+while(j < 51){
+    console.log(`o número é: ${j}`)
+    j++
+}
+console.log('\n')
